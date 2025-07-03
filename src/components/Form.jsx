6d7deb6a69e89842920d01886782d413aff5f6b6
Lincoln Sadler <lincolndiscord17@gmail.com>
2025-07-02 21:32:02 -0400

    Refactor form structure for cleaner layout

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,12 +5,8 @@ function Form() {
         <div className="form-container">
             <form>
                 <h1>Login</h1>
-                <div className="email-container">
-                    <input type="email" placeholder="E-mail" required />
-                </div>
-                <div className="password-container">
-                    <input type="password" placeholder="Password" required />
-                </div>
+                <input type="email" placeholder="E-mail" required />
+                <input type="password" placeholder="Password" required />
                 <button type="submit">Submit</button>
                 <div className="forgot-pass-container">
                     Forgot your password? <a href="#">Reset it here</a>
